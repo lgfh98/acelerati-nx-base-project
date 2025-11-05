6611b521eb548cae31717de03135833c2993c226
lgfh98 <lgfigueroah98@gmail.com>
2025-11-05 09:03:50 -0500

    new module: fundamentals

---
+++ apps/fundamentals/src/test/java/org/example/fundamentals/FundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
